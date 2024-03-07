01b90ad22805e090357b5a6b255252b23339f8f8
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-03-07 23:54:21 +0200

    ...Board List

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+interface BoardCardProps {
+  id: string;
+  title: string;
+  authorName: string;
+  authorId: string;
+  createdAt: number;
+  imageUrl: string;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+export const BoardCard = ({
+  authorId,
+  authorName,
+  createdAt,
+  id,
+  imageUrl,
+  isFavorite,
+  orgId,
+  title,
+}: BoardCardProps) => {
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+        <div className="relative flex-1 bg-amber-50">
+          <Image
+            src={imageUrl}
+            alt={title}
+            fill
+            className="object-fit opacity-10 p-2"
+          />
+        </div>
+      </div>
+    </Link>
+  );
+};
