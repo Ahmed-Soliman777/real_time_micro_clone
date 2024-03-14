393b2d1aae70c6fbfeeae6017727a9dbb7faed3e
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2024-03-14 05:37:50 +0200

    Cursors Presence

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -3,6 +3,7 @@ import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
 
 const client = createClient({
   // publicApiKey: "pk_dev_AFvOvDNYsCZxi-I2a9SeUTW06lSuNIJ0tZ-UdVL2YMB_auX5pH3fXBMAGEAdoxit",
+  throttle: 16,
   authEndpoint: "/api/liveblocks-auth",
   async resolveUsers({ userIds }) {
     // Used only for Comments and Notifications. Return a list of user information
@@ -50,7 +51,7 @@ const client = createClient({
 // and that will automatically be kept in sync. Accessible through the
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
-  // cursor: { x: number, y: number } | null,
+  cursor: { x: number; y: number } | null;
   // ...
 };
 
